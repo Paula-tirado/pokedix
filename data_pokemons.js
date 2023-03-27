305e15450ab34c5805a9a9e6786a642c5e536c86
Paula-tirado <120533705+Paula-tirado@users.noreply.github.com>
2023-03-27 14:31:51 -0500

    my proyect pokedix

---
+++ data_pokemons.js
@@ -0,0 +1,85 @@
+const pokemonGeneral =
+// pokemon
+
+[
+    {
+        "name": "bulbasaur",
+        "type": "grass",
+        "weight": 130,
+        "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        "attacks":[
+            { 
+                "name": "growl"
+                
+            },
+            {
+                "name": "superpower"
+            }
+        ],
+        "abilities": [
+            { 
+                "name": "overgrow",
+                "is_hidden": false
+            },
+            {
+                "name": "chlorophyll",
+                "is_hidden": true
+            }
+        ]
+    },
+    {
+        "name": "charmander",
+        "type": "fire",
+        "weight": 85,
+        "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg",
+        "attacks":[
+            { 
+                "name": "growl"
+                
+            },
+            {
+                "name": "superpower"
+            }
+        ],
+        "abilities": [
+            {
+               "name": "blaze",
+               "is_hidden": false
+            },
+            {
+                "name": "solar-power",
+                "is_hidden": true
+            }
+        ]
+    },
+    {
+        "name": "squirtle",
+        "type": "water", 
+        "weight": 90,
+        "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg",
+        "attacks":[
+            { 
+                "name": "growl"
+                
+            },
+            {
+                "name": "charm"
+            }
+        ],
+        "abilities": [
+            {
+                "name": "torrent",
+                "is_hidden": false
+            },
+            {
+                "name": "rain-dish",
+                "is_hidden": true
+            }
+        ]
+    }
+        
+]
+
+
+
+
---
+++ index.js
@@ -0,0 +1,77 @@
+ 
+  // const dataApiPokemon = require ("request");
+  // dataApiPokemon.get ("https://pokeapi.co/api/v2/", function (error,respuesta,body){
+    
+  // try {
+  //   if ( request.statusCode === 200){
+  //     const  jsonbody = JSON.parse(body);
+  //      console.log (jsonbody)
+  //       const data = rest.JSON
+  //        data.
+  //     // jsonbody.data.maps(())
+  //   }
+  // } catch (error) {
+  //   console.error(error)
+  // }  
+  // }const abilities = document.getElementById("abilidades")
+    // const abilidades  = abilities.map(({ abilities,}) => ({ [abilities]: value }));
+    // abilitiesDePokemon.innerHTML= pokemon.abilities 
+    // const abilitiesDePokemon = document.getElementById("abilidades");
+    // pokemonGeneral.map((pokemon)=>{
+    // abilitiesDePokemon.innerHTML= pokemon.abilities
+    
+  // )
+
+//   document.getElementById("iamgen-bulbasaur");
+ 
+
+const nombresDePokemon =  document.getElementById("nombres");
+
+  pokemonGeneral.map((pokemon)=>{ 
+  nombresDePokemon.innerHTML= pokemon.name
+ });
+//  console.log (document.getElementById("nombres"))
+  
+ const habilidadesPokemon= document.getElementById("abilidades");
+
+  pokemonGeneral.map((pokemon)=>{
+  habilidadesPokemon.innerHTML=pokemon.abilities
+
+  const recorridoHabilidades = document.getElementById("abilidades");
+    abilities.map ((pokemon)=>{
+    recorridoHabilidades= pokemon.abilities
+  })
+ })
+  // console.log (document.getElementById("abilidades"))
+  const atakesDePokemon = document.getElementById("atakes");
+
+  pokemonGeneral.map((pokemon)=>{
+    atakesDePokemon.innerHTML= pokemon.attacks
+  })
+
+//   console.log (pokemongeneral .find((pokemon)=>pokemon.name==="bulbasaur"))
+//  const bulbasaur = pokemongeneral .find((pokemon)=>pokemon.name==="bulbasaur")
+//  console.log (document.getElementById("iamgen-bulbasaur").src =bulbasaur.img)
+//  console.log (bulbasaur.img);
+
+
+ 
+
+//  const  elementCard = document.getElementById("card-pokemon");
+//   function duplicateElementCard (elemnt){
+//     pokemonGeneral.map (()=>{
+// // console.log(pokemon)
+//     })
+//   } 
+
+// duplicateElementCard( elementCard);
+
+
+  
+
+
+
+
+
+
+  
